983d7461cb9343a2421ce8c7644bf20b18593a77
Gustavo Pimenta <gustav.alves17@gmail.com>
2023-07-04 07:23:32 -0300

    commit necessario para não perder as mudanças

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,14 +1,15 @@
-from django.forms import ModelForm 
-from .models import Inventory 
+from django.forms import ModelForm
+from .models import Inventory
 
-class AddInventoryForm(ModelForm): 
-    class Meta: 
-        model = Inventory
-        fields = ['name', 'cost_per_item', 'quantity_in_stock','quantity_sold']
-        
-        
-        
-class UpdateInventoryForm(ModelForm): 
+
+class InventoryUpdateForm(ModelForm):
     class Meta:
         model = Inventory
-        fields = ['name', 'cost_per_item', 'quantity_in_stock','quantity_sold']
+        fields = ["name", "cost_per_item", "quantity_in_stock", "quantity_sold"]
+
+
+class AddInventoryForm(ModelForm):
+    class Meta:
+        model = Inventory
+        fields = ["name", "cost_per_item", "quantity_in_stock", "quantity_sold"]
+
