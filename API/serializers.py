7fdf186af2ccabb73a8550d0d961a7e2997a27c0
Gustavo Pimenta <gustav.alves17@gmail.com>
2023-07-04 06:39:16 -0300

    adicionado serializer e viewset

---
+++ API/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from Inventory import models
+
+class InventorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = 
+        fields = []
